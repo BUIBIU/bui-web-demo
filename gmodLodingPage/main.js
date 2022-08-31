1bb187cf76f0eb1d2c3ac2b885eaa22885fb7c9b
BUI <623607878@qq.com>
2022-08-31 08:34:52 +0800

    gmod event

---
+++ gmodLodingPage/main.js
@@ -0,0 +1,31 @@
+function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language) {
+    for (item of arguments) {
+        let div = document.createElement('div')
+        div.innerText = item
+        document.body.append(div)
+    }
+}
+function SetFilesTotal(total) {
+    let div = document.createElement('div')
+    div.innerText = total + " files"
+    document.body.append(div)
+}
+function SetFilesNeeded(needed) {
+    let div = document.createElement('div')
+    div.innerText = needed + " files"
+    document.body.append(div)
+}
+function DownloadingFile(fileName) {
+    let div = document.createElement('div')
+    div.innerText = fileName + " downloading"
+    document.body.append(div)
+}
+function SetStatusChanged(status) {
+    let div = document.createElement('div')
+    div.innerText = status + " status"
+    document.body.append(div)
+}
+window.onload = function () {
+
+
+}
