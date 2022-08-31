4929ab7c29797a4f6f330928bb057eae28f2d730
BUI <623607878@qq.com>
2022-08-31 11:12:21 +0800

    .

--- gmodLodingPage/main.js
+++ gmodLodingPage/main.js
@@ -1,33 +1,33 @@
-function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language) {
-    for (item of arguments) {
-        let div = document.createElement('div')
-        div.innerText = item
-        document.body.append(div)
-    }
-}
-function SetFilesTotal(total) {
-    let div = document.createElement('div')
-    div.innerText = total + " total files"
-    document.body.append(div)
-}
-function SetFilesNeeded(needed) {
-    let div = document.createElement('div')
-    div.innerText = needed + " files need"
-    document.body.append(div)
-}
-function DownloadingFile(fileName) {
-    let div = document.createElement('div')
-    div.innerText = fileName + " downloading"
-    document.body.append(div)
-}
-function SetStatusChanged(status) {
-    let div = document.createElement('div')
-    div.innerText = status + " status"
-    document.body.append(div)
-}
-window.onload = function () {
-    let div = document.createElement('div')
-    div.innerText = navigator.userAgent
-    document.body.append(div)
+// function GameDetails(servername, serverurl, mapname, maxplayers, steamid, gamemode, volume, language) {
+//     for (item of arguments) {
+//         let div = document.createElement('div')
+//         div.innerText = item
+//         document.body.append(div)
+//     }
+// }
+// function SetFilesTotal(total) {
+//     let div = document.createElement('div')
+//     div.innerText = total + " total files"
+//     document.body.append(div)
+// }
+// function SetFilesNeeded(needed) {
+//     let div = document.createElement('div')
+//     div.innerText = needed + " files need"
+//     document.body.append(div)
+// }
+// function DownloadingFile(fileName) {
+//     let div = document.createElement('div')
+//     div.innerText = fileName + " downloading"
+//     document.body.append(div)
+// }
+// function SetStatusChanged(status) {
+//     let div = document.createElement('div')
+//     div.innerText = status + " status"
+//     document.body.append(div)
+// }
 
+onload = function () {
+    
 }
+
+window.onload = onload
